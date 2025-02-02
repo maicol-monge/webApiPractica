9b24e576109dacced7dbc4d6512f3538dfa1f363
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-02-02 16:24:32 -0600

    Se hicieron los metodos del CRUD

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -29,6 +29,111 @@ namespace webApiPractica.Controllers
 
             return Ok(listadoEquipo);
         }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos  == id
+                               select e).FirstOrDefault();
+
+            if(equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+
+        
+        [HttpGet]
+        [Route("Find/{filtro}")]
+        public IActionResult FindByDescription(string filtro)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+            try
+            {
+                _equiposContexto.equipos.Add(equipo);
+                _equiposContexto.SaveChanges();
+                return Ok(equipo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+            equipos? equipoActual = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+
+
+
+            if (equipoActual == null)
+            {
+                return NotFound();
+            }
+
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+
+
+            return Ok(equipoModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarEquipos(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                                     where e.id_equipos == id
+                                     select e).FirstOrDefault();
+
+
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            
+
+            _equiposContexto.equipos.Attach(equipo);
+            _equiposContexto.equipos.Remove(equipo);
+            _equiposContexto.SaveChanges();
+
+
+
+            return Ok(equipo);
+        }
     }
 
 }
